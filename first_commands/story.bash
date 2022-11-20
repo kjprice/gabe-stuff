1cdb01ee5edad7c7c4372e8a24d32d48decaabfb
KJ Price <kjprice12@gmail.com>
2022-11-20 10:09:33 -0600

    Add delay between story lines

--- first_commands/story.bash
+++ first_commands/story.bash
@@ -3,11 +3,16 @@
 
 
 echo there once was a girl
+sleep 1
 
 echo She went to the bathroom
+sleep 1
 
 echo But then there was a shark
+sleep 1
 
 echo The shark eats the girl
+sleep 1
 
 echo They lived happily ever after
+sleep 1
