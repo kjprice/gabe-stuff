a1bdced2e73691572b66a7d8b1ebf0a8345607b0
KJ Price <kjprice12@gmail.com>
2022-11-20 10:22:01 -0600

    Variables introduction

---
+++ first_commands/variables.bash
@@ -0,0 +1,14 @@
+first_name=nothing
+last_name=nothing
+
+echo I have first name as $first_name and last name as $last_name
+
+echo what is your first name?
+read first_name
+
+echo I have first name as $first_name and last name as $last_name
+
+echo what is your last name?
+read last_name
+
+echo I have first name as $first_name and last name as $last_name
