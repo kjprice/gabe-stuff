86940c0b90dfc6524f729a937df9bc5f5ef1778d
KJ Price <kjprice12@gmail.com>
2022-11-20 10:03:58 -0600

    First story - just output

---
+++ first_commands/story.bash
@@ -0,0 +1,13 @@
+
+# This is a story about a girl who walks into a bathroom and gets eaten by a shark
+
+
+echo there once was a girl
+
+echo She went to the bathroom
+
+echo But then there was a shark
+
+echo The shark eats the girl
+
+echo They lived happily ever after
