af9134ba0954863e0480a7ba52f720a7757784b4
KJ Price <kjprice12@gmail.com>
2022-11-21 14:31:49 -0600

    First python story

---
+++ first_commands/first_python_story.py
@@ -0,0 +1,18 @@
+import time
+
+print('echo there once was a girl')
+time.sleep(1)
+
+# She went to the bathroom
+print('She went to the bathroom')
+time.sleep(1)
+# But then there was a shark
+print('But then there was a shark')
+time.sleep(1)
+
+# Does the shark eat the girl? y/n
+# The shark eats the girl
+
+# They lived happily ever after
+print('They lived happily ever after')
+time.sleep(1)
