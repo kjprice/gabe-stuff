483fef8266be49e17add7a25e3aeb99be92bb7a4
KJ Price <kjprice12@gmail.com>
2022-11-21 14:34:44 -0600

    Remove comments

--- first_commands/first_python_story.py
+++ first_commands/first_python_story.py
@@ -3,10 +3,9 @@ import time
 print('echo there once was a girl')
 time.sleep(1)
 
-# She went to the bathroom
 print('She went to the bathroom')
 time.sleep(1)
-# But then there was a shark
+
 print('But then there was a shark')
 time.sleep(1)
 
@@ -15,6 +14,5 @@ if does_shark_eats_girl == 'y':
   print('The shark eats the girl')
   time.sleep(1)
 
-# They lived happily ever after
 print('They lived happily ever after')
 time.sleep(1)
