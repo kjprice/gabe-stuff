291d957bc3d4b23f0b4ce8e8826c72d5240083d3
KJ Price <kjprice12@gmail.com>
2022-11-22 16:50:49 -0600

    Family story python

---
+++ story_commands_python/family_story.py
@@ -0,0 +1,8 @@
+
+print('ones upon a time ther  lived a hapy famly')
+
+
+mom_name = input('ther was a mom namde ')
+dad_name = input('ther was a dad namde ')
+
+print(f'{mom_name} and {dad_name} love each other')
